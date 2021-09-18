12d82c227a7863d98acdc5bc7a73734399e91c31
borjaprieto <borja.prieto@gmail.com>
2021-09-18 11:17:11 +0000

    svg dashboard icons for custom types

--- includes/vendor.php
+++ includes/vendor.php
@@ -139,7 +139,7 @@ function fyv_vendor_init() {
 		'map_meta_cap' => true,
 	//	'capability_type' => [ 'vendor', 'vendors' ],
 		'capability_type'    => 'post',
-		'menu_icon' => 'dashicons-building',
+		'menu_icon' => 'data:image/svg+xml;base64,' . base64_encode('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="black" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm95.8 32.6L272 480l-32-136 32-56h-96l32 56-32 136-47.8-191.4C56.9 292 0 350.3 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-72.1-56.9-130.4-128.2-133.8z"/></svg>' ),
 	];
 
 	register_post_type( 'vendor', $args );
